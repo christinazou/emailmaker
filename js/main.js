d3a6eca049a0cac5a5a889a855579edce2d305f9
christinazou <xinguanzxk@126.com>
2016-02-07 01:15:10 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function() {
+  $("#select_all_btn").click(function() {
+    $(".email").select();
+  })
+});
+$(function() {
+  $(".greeting h3").bind("click", function() {
+    $(".greeting div").toggle("slow");
+  });
+});
+$(function() {
+  $(".reason h3").bind("click", function() {
+    $(".reason div").toggle("slow");
+  });
+});
+$(function() {
+  $(".requesting h3").bind("click", function() {
+    $(".requesting div").toggle("slow");
+  });
+});
+$(function() {
+  $(".ending h3").bind("click", function() {
+    $(".ending div").toggle("slow");
+  });
+});
+$(function() {
+  $(".closing h3").bind("click", function() {
+    $(".closing div").toggle("slow");
+  });
+});
+
+$("input").change(function() {
+  var etxt = ""
+  if ($("input[name=1]:checked").val() !== undefined) {
+    etxt = etxt + $("input[name=1]:checked").val() + "\n\n"
+  }
+  if ($("input[name=2]:checked").val() !== undefined) {
+    etxt = etxt + $("input[name=2]:checked").val()
+  }
+  if ($("input[name=3]:checked").val() !== undefined) {
+    etxt = etxt + $("input[name=3]:checked").val()
+  }
+  if ($("input[name=4]:checked").val() !== undefined) {
+    etxt = etxt + $("input[name=4]:checked").val()
+  }
+  if ($("input[name=5]:checked").val() !== undefined) {
+    etxt = etxt + "\n\n" + $("input[name=5]:checked").val()
+  }
+  $(".email").text(etxt);
+});
