b731ec8623761da822073a67ecadf82d5b98cb79
christinazou <xinguanzxk@126.com>
2016-02-21 23:27:51 -0500

    update main.js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
         $.each(data, function(i1, catagory) {
             items.push("<div class=" + "'" + i1 + "'>");
             for (var i = 0; i <= catagory.length - 1; i++) {
-                x=catagory[i];
+                var x = catagory[i];
                 // strHtml = strHtml + "index: " + i + "value: " + catagory[i];
                 if (i = 0) {
                     items.push("<h3>" + x.cname + "</h3>");
                     items.push("<ul>");
-                } else if (i < catagory.length - 1) {
+                } else if (i > 0 && i < catagory.length - 1) {
                     items.push("<li>" + "<input type='radio' name='" + x.radioname + " id=" + x.val + " value=" + x.val + " class='" + x.ctype + "' /><label for=" + x.val + ">" + x.showtext + "</label>" + "</li>");
                     //  strHtml = strHtml + "index: " + j + "value: " + value;
                     // body...
@@ -26,64 +26,64 @@ $(document).ready(function() {
             html: items.join("")
         }).appendTo($jsondiv);
     });
-$(function() {
-    $("#select_all_btn").click(function() {
-        $(".email").select();
-    })
-});
-$(function() {
-    $(".greeting h3").bind("click", function() {
-        $(".greeting div").toggle("slow");
+    $(function() {
+        $("#select_all_btn").click(function() {
+            $(".email").select();
+        })
     });
-});
-$(function() {
-    $(".reason h3").bind("click", function() {
-        $(".reason div").toggle("slow");
+    $(function() {
+        $(".greeting h3").bind("click", function() {
+            $(".greeting div").toggle("slow");
+        });
     });
-});
-$(function() {
-    $(".requesting h3").bind("click", function() {
-        $(".requesting div").toggle("slow");
+    $(function() {
+        $(".reason h3").bind("click", function() {
+            $(".reason div").toggle("slow");
+        });
     });
-});
-$(function() {
-    $(".offering h3").bind("click", function() {
-        $(".offering div").toggle("slow");
+    $(function() {
+        $(".requesting h3").bind("click", function() {
+            $(".requesting div").toggle("slow");
+        });
     });
-});
-$(function() {
-    $(".ending h3").bind("click", function() {
-        $(".ending div").toggle("slow");
+    $(function() {
+        $(".offering h3").bind("click", function() {
+            $(".offering div").toggle("slow");
+        });
     });
-});
-$(function() {
-    $(".closing h3").bind("click", function() {
-        $(".closing div").toggle("slow");
+    $(function() {
+        $(".ending h3").bind("click", function() {
+            $(".ending div").toggle("slow");
+        });
     });
-});
+    $(function() {
+        $(".closing h3").bind("click", function() {
+            $(".closing div").toggle("slow");
+        });
+    });
 
-$("input").change(function() {
-var etxt = ""
-if ($("input[name=1]:checked").val() !== undefined) {
-    if ($("input[name=1]:checked").val() === "Hi" || $("input[name=1]:checked").val() === "Hello") {
-        if ($(".to").val() === null || $(".to").val() === "") {
-            etxt = etxt + $("input[name=1]:checked").val() + ",\n\n"
-        } else {
-            etxt = etxt + $("input[name=1]:checked").val() + " " + $(".to").val() + ",\n\n";
+    $("input").change(function() {
+        var etxt = ""
+        if ($("input[name=1]:checked").val() !== undefined) {
+            if ($("input[name=1]:checked").val() === "Hi" || $("input[name=1]:checked").val() === "Hello") {
+                if ($(".to").val() === null || $(".to").val() === "") {
+                    etxt = etxt + $("input[name=1]:checked").val() + ",\n\n"
+                } else {
+                    etxt = etxt + $("input[name=1]:checked").val() + " " + $(".to").val() + ",\n\n";
+                }
+            } else {
+                etxt = etxt + $("input[name=1]:checked").val() + "\n\n"
+            }
         }
-    } else {
-        etxt = etxt + $("input[name=1]:checked").val() + "\n\n"
-    }
-}
 
-for (var i = 2; i >= 8; i++) {
-    if ($("input[name=i]:checked").val() !== undefined) {
-        etxt = etxt + $("input[name=i]:checked").val()
-    }
-};
-if ($("input[name=6]:checked").val() !== undefined) {
-    etxt = etxt + "\n\n" + $("input[name=5]:checked").val()
-}
-$(".email").text(etxt);
-});
+        for (var i = 2; i >= 8; i++) {
+            if ($("input[name=i]:checked").val() !== undefined) {
+                etxt = etxt + $("input[name=i]:checked").val()
+            }
+        };
+        if ($("input[name=6]:checked").val() !== undefined) {
+            etxt = etxt + "\n\n" + $("input[name=5]:checked").val()
+        }
+        $(".email").text(etxt);
+    });
 });
